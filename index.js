3ecceeb357dbc66efb8c766b983c94120f23d93b
Paweł Mroczka <pawel.mroczka@neti-soft.com>
2023-08-22 17:26:15 +0200

    change credentials

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(logger);
 app.use(credentials);
 
 // Cross Origin Resource Sharing
-app.use(cors());
+app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
